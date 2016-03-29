65a1bf8f536a33fefd310e80dd35a526f3011f8e
saadq <saad@saadquadri.com>
2016-03-29 03:36:04 -0400

    Finish getShifts()

--- src/shifts/get-shifts.js
+++ src/shifts/get-shifts.js
@@ -1,15 +1,26 @@
 import { tableContainer } from './nodes'
 
 function getShifts() {
+  const shifts = []
   const table = tableContainer.children[0]
-  const tBody = table.getElementsByTagName('tbody')[0]
-  const rows = Array.from(tBody.getElementsByTagName('tr'))
+  const tbody = table.getElementsByTagName('tbody')[0]
+  const rows = Array.from(tbody.getElementsByTagName('tr'))
 
   rows.forEach(row => {
-    console.log(row)
+    const cells = Array.from(row.getElementsByTagName('td'))
+    const [ date, clockIn, clockOut, duration ] = cells.slice(4)
+
+    const shift = {
+      date: date.textContent,
+      clockIn: clockIn.textContent,
+      clockOut: clockOut.textContent,
+      duration: duration.textContent
+    }
+
+    shifts.push(shift)
   })
 
-  const durationIndex = 8
+  return shifts
 }
 
 export default getShifts

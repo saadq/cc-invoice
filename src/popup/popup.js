0df6eeea17473ff2f53a10b7394379321039f159
saadq <saad@saadquadri.com>
2016-04-01 08:44:08 -0400

    Improve popup preference changes

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,9 +1,32 @@
 const $ = selector => document.querySelector(selector)
 const $button = $('#save')
+const $status = $('#status')
+const $email = $('#email')
+const $address = $('#address')
 
 $button.addEventListener('click', () => {
-  const email = $('#email').value
-  const address = $('#address').value
+  const email = $email.value
+  const address = $address.value
 
-  chrome.storage.sync.set({ email, address })
+  let message
+
+  if (address && email) {
+    chrome.storage.sync.set({ email, address })
+    message = 'Your address and email settings have been saved!'
+  }
+
+  else if (address) {
+    chrome.storage.sync.set({ address })
+    message = 'Your address settings have been saved!'
+  }
+
+  else if (email) {
+    chrome.storage.sync.set({ email })
+    message = 'Your email settings have been saved!'
+  }
+
+  if (message) {
+    $status.style.color = 'green'
+    $status.textContent = message
+  }
 })
